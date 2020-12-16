34f770608656ebb63c2da8dd9107469c4bedf131
Raphaël H <github@hraph.fr>
2020-12-16 16:01:09 +0000

    Add missing repositories

---
+++ src/Repository/PaygreenShopRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hraph\SyliusPaygreenPlugin\Repository;
+
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use SyliusLabs\AssociationHydrator\AssociationHydrator;
+
+class PaygreenShopRepository extends EntityRepository
+{
+    /** @var AssociationHydrator */
+    private AssociationHydrator $associationHydrator;
+
+    public function __construct(EntityManager $entityManager, Mapping\ClassMetadata $class)
+    {
+        parent::__construct($entityManager, $class);
+
+        $this->associationHydrator = new AssociationHydrator($entityManager, $class);
+    }
+}
